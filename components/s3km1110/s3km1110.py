526f8efd1847a473b1a256508a602467661ce340
ryangjpriorx <ryangjpriorx@gmail.com>
2025-12-28 21:53:48 -0500

    Update s3km1110.py

--- components/s3km1110/s3km1110.py
+++ components/s3km1110/s3km1110.py
@@ -1,9 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import uart
-from esphome.const import (
-    CONF_ID,
-)
+from esphome.const import CONF_ID
 
 CODEOWNERS = ["@ryangjpriorx"]
 
